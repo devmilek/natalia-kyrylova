8d22a78dfb15dfc5aaed7fd7db24b5d53b8c4c02
Miłosz Kraiński <miloszkrianski@gmail.com>
2023-06-28 20:13:37 +0200

    CREATE: Simple gallery

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
-
 import image from "@astrojs/image";
+
+import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image()],
+  integrations: [tailwind(), image(), vue()],
   output: "server",
   adapter: vercel()
 });
---
+++ src/components/Gallery.vue
@@ -0,0 +1,49 @@
+<template>
+  <div
+    class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mt-10"
+  >
+    <div
+      v-for="photo in optImages"
+      class="overflow-hidden rounded-lg cursor-pointer"
+    >
+      <img
+        class="w-full h-full hover:scale-110 transition-all"
+        :src="photo"
+        alt="Zdjęcie galerii"
+      />
+    </div>
+    <div class="absolute right-0 top-0 z-[100]">asdasdasd</div>
+  </div>
+</template>
+
+<script setup>
+defineProps(["photos", "test"]);
+
+import { getImage } from "@astrojs/image";
+
+const images = [
+  "gal1.jpg",
+  "gal2.jpg",
+  "gal3.jpg",
+  "gal4.jpg",
+  "gal5.jpg",
+  "gal6.jpg",
+];
+
+const optImages = [];
+
+images.forEach(async (image) => {
+  const { src } = await getImage({
+    src: import("../images/gallery/" + image),
+    alt: "My hero image",
+  });
+  optImages.push(src);
+});
+
+const { src } = await getImage({
+  src: import("../images/natalia1.jpg"),
+  alt: "My hero image",
+});
+
+// console.log(optImages);
+</script>
