d80eba5b9ffc4b9adcd33aa4e722c2f4357dd392
Miłosz Kraiński <miloszkrianski@gmail.com>
2023-06-28 17:51:28 +0200

    FIX: hero image size

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import vercel from "@astrojs/vercel/serverless";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), image()],
   output: "server",
   adapter: vercel()
 });
