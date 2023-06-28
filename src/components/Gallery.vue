6d999ac5355418a15de2198f4715b806358afe80
Miłosz Kraiński <miloszkrianski@gmail.com>
2023-06-28 21:17:25 +0200

    create gallery and links

--- src/components/Gallery.vue
+++ src/components/Gallery.vue
@@ -2,9 +2,11 @@
   <div
     class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mt-10"
   >
+    {{ selectedPhoto }}
     <div
       v-for="photo in optImages"
       class="overflow-hidden rounded-lg cursor-pointer"
+      @click="selectedPhoto = photo"
     >
       <img
         class="w-full h-full hover:scale-110 transition-all"
@@ -12,14 +14,23 @@
         alt="Zdjęcie galerii"
       />
     </div>
-    <div class="absolute right-0 top-0 z-[100]">asdasdasd</div>
+    <div
+      class="absolute right-0 top-0 z-[100] w-full h-full flex items-center justify-center bg-neutral-900/40"
+      @click="selectedPhoto = ''"
+      v-if="selectedPhoto"
+    >
+      <div class="max-w-4xl bg-white w-full">{{ selectedPhoto }}</div>
+    </div>
   </div>
 </template>
 
 <script setup>
+import { ref } from "vue";
 defineProps(["photos", "test"]);
 
 import { getImage } from "@astrojs/image";
+
+const selectedPhoto = ref("");
 
 const images = [
   "gal1.jpg",
